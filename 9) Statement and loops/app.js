cf840721a460a727c1764658b0bbd7120a793525
Isha Ganar <ishaganar15@gmail.com>
2024-01-08 18:04:19 +0530

    Isha Leaned if else and Switch Statements

---
+++ 9) Statement and loops/app.js
@@ -0,0 +1,65 @@
+console.log('Learning Loops & Statements');
+
+
+// JavaScript if statement
+var hour = new Date().getHours();
+if (hour < 16) {
+    greeting = "Good day";
+    console.log(greeting);
+}
+// Javascript if else statement
+if (hour < 16) {
+    greeting = "Good day";
+    console.log(greeting);
+} else {
+    greeting = "Good evening";
+    console.log(greeting);
+}
+//else if Statement
+if (hour < 10) {
+    greeting = "Good morning";
+    console.log(greeting);
+} else if (hour < 20) {
+    greeting = "Good day";
+    console.log(greeting);
+} else {
+    greeting = "Good evening";
+    console.log(greeting);
+}
+
+
+// JavaScript Switch Statement
+var day = new Date().getDay();
+console.log(day);
+switch (day) {
+    case 1:
+        day = "Today is Monday"
+        break;
+    case 2:
+        day = "Today is tuesday"
+    case 3:
+        day = "Today is Wednesday"
+    case 4:
+        day = "Today is Thursday"
+    case 5:
+        day = "Today is Friday"
+    default:
+        text = "Its Weekend";
+}
+console.log(day);
+
+//Common Code Blocks
+var text;
+switch (new Date().getDay()) {
+    case 4:
+    case 5:
+      text = "Soon it is Weekend";
+      break;
+    case 0:
+    case 6:
+      text = "It is Weekend";
+      break;
+    default:
+      text = "Looking forward to the Weekend";
+  }
+  console.log(text);
