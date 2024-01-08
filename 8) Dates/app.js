c9d348eb3588d97f8a89fe98fdf21ec8df45bbb5
Isha Ganar <ishaganar15@gmail.com>
2024-01-08 17:06:30 +0530

    Isha Learned about Date Object

---
+++ 8) Dates/app.js
@@ -0,0 +1,14 @@
+console.log('Learning JavaScript Date Objects !! ');
+
+
+var date_1 = new Date();
+console.log(date_1);
+
+var date_2 = new Date("1998-08-15");
+console.log(date_2);
+
+const d = new Date("2022-03-25");
+console.log(d);
+
+const dt = new Date(2018, 05, 69, 10, 33, 30, 0);
+console.log(dt);
