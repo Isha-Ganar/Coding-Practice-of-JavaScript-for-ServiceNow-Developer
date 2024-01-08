f353bebfc3a41fc1edbb02fe3ab6e9debfd02e26
Isha Ganar <ishaganar15@gmail.com>
2024-01-08 18:28:01 +0530

    Isha Learned about loop

--- 9) Statement and loops/app.js
+++ 9) Statement and loops/app.js
@@ -53,13 +53,35 @@ var text;
 switch (new Date().getDay()) {
     case 4:
     case 5:
-      text = "Soon it is Weekend";
-      break;
+        text = "Soon it is Weekend";
+        break;
     case 0:
     case 6:
-      text = "It is Weekend";
-      break;
+        text = "It is Weekend";
+        break;
     default:
-      text = "Looking forward to the Weekend";
-  }
-  console.log(text);
+        text = "Looking forward to the Weekend";
+}
+console.log(text);
+
+
+// loops
+//For Loop:
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+//While Loop:
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+
+var i = 374;
+do {
+    console.log(i);
+    i++;
+} while (i < 365);
+
