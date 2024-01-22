976ab0cdaf1eb21eeffa96ba891effd27acdbb03
Isha Ganar <ishaganar15@gmail.com>
2024-01-22 14:56:07 +0530

    a small change in Average Function

--- Asst/app.js
+++ Asst/app.js
@@ -25,9 +25,11 @@ mySort(myArr);
 function Average(arr){
 var sum = 0;
 for(i=0;i<arr.length;i++){
-    sum = sum + i
+    sum = sum + arr[i]
 }
+
 var avg = sum/(arr.length);
+
 console.log(avg);
 }
 
